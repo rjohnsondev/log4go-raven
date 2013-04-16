f70bb1061f279b33b9f02011bd95b814d82dc44b
Richard Johnson <github@rjohnson.id.au>
2013-04-16 15:21:28 +0100

    Allow up to 32 simultaneous requests to help speed up processing

--- raven.go
+++ raven.go
@@ -10,6 +10,11 @@ import (
     "github.com/rjohnsondev/raven-go/raven"
 )
 
+var (
+    // number of simultaneous requests to send through to sentry
+    RavenConcurrencyLevel = 32
+)
+
 // This is the standard writer that prints to standard output.
 type RavenLogWriter chan *LogRecord
 
@@ -20,50 +25,54 @@ func NewRavenLogWriter(dsn string) RavenLogWriter {
 		fmt.Fprintf(os.Stderr, "Unable to determine hostname, logging to root\n")
     }
 
-    client, err := raven.NewClient(dsn, hostname)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "NewRavenLogWriter(%q): %s\n", dsn, err)
-		return nil
-	}
-
 	w := RavenLogWriter(make(chan *LogRecord, LogBufferLength))
 
-	go func() {
-        var timestr string
-        var timestrAt int64
-
-		for rec := range w {
-            if at := rec.Created.UnixNano() / 1e9; at != timestrAt {
-                timestr, timestrAt = rec.Created.Format("01/02/06 15:04:05"), at
-            }
-
-            extra := make(map[string]interface{})
-            extra["StackTrace"] = strings.Split(strings.Replace(string(rec.Stack), "\t", "    ",-1), "\n")
-            extra["Time"] = timestr
-            extra["Level"] = rec.Level
-            extra["Source"] = rec.Source
-
-            var err error = nil
-            if rec.Level <= DEBUG {
-                err = client.Debug(rec.Message, extra)
-            } else if rec.Level <= INFO {
-                err = client.Info(rec.Message, extra)
-            } else if rec.Level <= WARNING {
-                err = client.Warning(rec.Message, extra)
-            } else if rec.Level <= ERROR {
-                err = client.Error(rec.Message, extra)
-            } else if rec.Level <= CRITICAL {
-                err = client.Fatal(rec.Message, extra)
-            }
-
-			if err != nil {
-				fmt.Printf("RavenLogWriter(%q): %s \n %s", dsn, err, rec.Message)
-				return
-			}
-		}
-	}()
+    for x := 0; x < RavenConcurrencyLevel; x++ {
+        go ravenLogWorker(w, dsn, hostname)
+    }
 
 	return w
+}
+
+func ravenLogWorker(w RavenLogWriter, dsn string, hostname string) {
+    client, err := raven.NewClient(dsn, hostname)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "NewRavenLogWriter(%q): %s\n", dsn, err)
+        return
+    }
+
+    var timestr string
+    var timestrAt int64
+
+    for rec := range w {
+        if at := rec.Created.UnixNano() / 1e9; at != timestrAt {
+            timestr, timestrAt = rec.Created.Format("01/02/06 15:04:05"), at
+        }
+
+        extra := make(map[string]interface{})
+        extra["StackTrace"] = strings.Split(strings.Replace(string(rec.Stack), "\t", "    ",-1), "\n")
+        extra["Time"] = timestr
+        extra["Level"] = rec.Level
+        extra["Source"] = rec.Source
+
+        var err error = nil
+        if rec.Level <= DEBUG {
+            err = client.Debug(rec.Message, extra)
+        } else if rec.Level <= INFO {
+            err = client.Info(rec.Message, extra)
+        } else if rec.Level <= WARNING {
+            err = client.Warning(rec.Message, extra)
+        } else if rec.Level <= ERROR {
+            err = client.Error(rec.Message, extra)
+        } else if rec.Level <= CRITICAL {
+            err = client.Fatal(rec.Message, extra)
+        }
+
+        if err != nil {
+            fmt.Printf("RavenLogWriter(%q): %s \n %s", dsn, err, rec.Message)
+            return
+        }
+    }
 }
 
 // This is the ConsoleLogWriter's output method.  This will block if the output
